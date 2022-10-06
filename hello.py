5a099cb4e2a9a9d9e376e04ad6ef201138700e02
syoungbin <youngbin0428@naver.com>
2022-10-06 12:57:54 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
+from curses import has_extended_color_support
+
+
 def print_hello():
 print("Hello World!!")
+
+hello,hello,hello,hello,hello,hello,hello,hello.
 
 if __name__ == '__main__':
 print_hello()
