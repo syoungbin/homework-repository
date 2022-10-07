ee4913f64a5309bc2df4c899f3c90fada3f9b9d2
syoungbin <youngbin0428@naver.com>
2022-10-07 13:36:45 +0900

    add

---
+++ prime_number.py
@@ -0,0 +1,40 @@
+import random
+class Test:
+    def addTest(self):
+        a = random.randint(0, 10)
+        b = random.randint(0, 10)
+        c = a + b
+        print(a, "+", b, "= ", end="")
+        ans = int(input())
+        if ans == c:
+            return 0
+    def subTest(self):
+        a = random.randint(0, 10)
+        b = random.randint(0, 10)
+        c = a - b
+        print(a, "-", b, "= ", end="")
+        ans = int(input())
+        if ans == c:
+            return 1
+        else:
+            return 0
+        
+score = 0
+
+test = Test()
+
+while True:
+    
+    print("-----")
+    print("[1] add")
+    print("[2] substract")
+    print("[q] exit")
+    print("-----")
+    m = input("select menu:")
+    if (m=='q'):
+        break
+    elif (m=='1'):
+        score = score + test.addTest()
+    elif (m=='2'):
+        score = score + test.subTest()
+print("Your score is", score)
